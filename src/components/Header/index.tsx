8426e6533b1bef7c5129f9f3704ec46ed808e3be
guilhermefrag <guilhermefragnani@icloud.com>
2023-03-02 15:18:56 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { UilApple } from '@iconscout/react-unicons';
+import PropTypes from 'prop-types';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <UilApple /> Apple
+      </h1>
+    </header>
+  )
+}
+
+export default Header
