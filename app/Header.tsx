7c88e26526e66a66091a01d11e196852e8f454b1
guilhermefrag <guilhermefragnani@icloud.com>
2023-03-06 10:59:49 -0300

    recriando como next app

---
+++ app/Header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <div className="p-5 bg-blue-500">I am Header</div>
+  )
+}
+
+export default Header
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="text-red-600">
+      <h1>Hello World! Home</h1>
+    </div>
+  )
+}
+
+export default Home
