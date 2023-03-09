16ed4c3190bf84fa39b811cd9234724714ee71bf
guilhermefrag <guilhermefragnani@icloud.com>
2023-03-09 14:57:17 -0300

    creating header

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -6,9 +6,9 @@ export default function SearchBar() {
 			<input
 				type="text"
 				placeholder="Search for a city..."
-				className="border-black border-2 rounded-md h-10 w-96 pl-2"
+				className="border-black border-2 rounded-md md:h-10 md:w-96 md:pl-2 h-9 w-[100%] pl-2"
 			/>
-			<div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+			<div className="absolute inset-y-0 md:right-2 pr-0 flex items-center pointer-events-none right-4">
 				<UilSearchAlt />
 			</div>
 		</div>
