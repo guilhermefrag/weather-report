4acfaf45c1083e36619a6446bad854d6f35768db
guilhermefrag <guilhermefragnani@icloud.com>
2023-03-14 13:20:41 -0300

    adicionando o banco de aparecida goiania

--- src/app/api/weather/[id]/[cityName]/route.ts
+++ src/app/api/weather/[id]/[cityName]/route.ts
@@ -1,5 +1,5 @@
 export async function GET(request: Request, { params }) {
-  const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${process.env.GEONAMES_USERNAME}`)
+  const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=-15.79159&lon=-47.89558&appid=${process.env.WEATHER_API_KEY}`)
   
   return new Response(JSON.stringify(await res.json()))
 }
